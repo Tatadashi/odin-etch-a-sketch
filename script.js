cfa4cd456c093538596be43de7bc884eff0d02db
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-10-28 04:52:39 -0700

    Create a 16x16 grid of square divs
    Create 16 containers (row) which each contains a container (col) with 16 identical square divs. The squares all maintain the aspect ratio of a square when shrinking and (not yet) expands if there is extra room on screen. The body covers the whole viewport and there is a minimum size to the grid.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 function createGrid () {
+    //create 1 square div
     const squareDiv = document.createElement(`div`);
-    squareDiv.style.cssText = `border: 5px solid; border-color: blue; background-color: red; min-width: 50px; min-height: 50px; display: flex; flex: 1;`;
-    
-    const gridContainer = document.querySelector(`#gridContainer`);
-    for (let i = 0; i < 16; i++) {
-        const squareDivClone = squareDiv.cloneNode(squareDiv);
-        squareDivClone.textContent = i + 1;        
-        gridContainer.appendChild(squareDivClone);
-    }
+    squareDiv.style.cssText = `border: 5px solid; border-color: blue; background-color: red; min-width: 15px; aspect-ratio: 1/1; display: flex; flex: 1;`;
+
+    //create 16 rows (horizontal) each containing a column (vertical) of 16 square divs, WIP grid can only shrink not grow 
+    const container = document.querySelector(`#gridContainer`);
+    for (i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+  
+        for (j = 0; j < 16; j++) {
+            const col = document.createElement("div");
+            const colSquare = squareDiv.cloneNode();
+            colSquare.textContent = `j`;
+            col.appendChild(colSquare);
+            row.appendChild(col);
+        }
+
+        container.appendChild(row);
+    }    
 }
 
 createGrid();
