3c9a0f91b9a0c77dc0ce3c6f3eadde916746b948
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-10-30 02:01:30 -0700

    Finish grid scalability and remove temp elements
    Remove most flexbox properties from gridContainer and squareDiv in favor adding them to row. Remove temp text and colors. Remove fixed height for body as flexbox property of gridContainer should expand grid to full screen (although one may have to scroll down)

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function createGrid () {
     //create 1 square div
     const squareDiv = document.createElement(`div`);
-    squareDiv.style.cssText = `border: 5px solid; border-color: blue; background-color: red; min-width: 15px; aspect-ratio: 1/1; display: flex; flex: 1;`;
+    squareDiv.style.cssText = `border: 2px solid; border-color: black; min-width: 15px; aspect-ratio: 1/1;`;
 
     //create 16 rows (horizontal) each containing a column (vertical) of 16 square divs, WIP grid can only shrink not grow 
     const container = document.querySelector(`#gridContainer`);
     for (i = 0; i < 16; i++) {
         const row = document.createElement("div");
         row.className = "row";
+        row.style.cssText = `display: flex; flex: 1; flex-direction: column;`;
   
         for (j = 0; j < 16; j++) {
             const col = document.createElement("div");
             const colSquare = squareDiv.cloneNode();
-            colSquare.textContent = `j`;
             col.appendChild(colSquare);
             row.appendChild(col);
         }
