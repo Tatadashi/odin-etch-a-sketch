cc8286ceae97237b3a77ddb26b5b444174e0888d
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-11-06 12:31:30 -0800

    Change makeGrid() to allow for input value of rows
    Add parameter (default is 16). Replace all previous instances of 16 with either the parameter value or the constant which is set to that value. Change minimum width of a square div to 3 to accomodate the max input of 100. Also change constant associated with the minimum width

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function createGrid () {
+function createGrid (squareCount = 16) {
     //create 1 square div
     const squareDiv = document.createElement(`div`);
-    squareDiv.style.cssText = `border: 1px solid; border-color: black; min-width: 10px; background-color: white; aspect-ratio: 1/1;`;
+    squareDiv.style.cssText = `border: 1px solid; border-color: black; min-width: 3px; background-color: white; aspect-ratio: 1/1;`;
     squareDiv.classList.add(`box`);
 
     //find the smaller viewport dimension in order to calculate maximum size of 1 square div in the grid
@@ -13,22 +13,22 @@ function createGrid () {
     smallerViewportDimension -= CONTAINER_PADDING_PIXELS;
 
     //get the biggest integer that a square div can be with the viewport
-    const NUMBER_OF_SQUARES_IN_ROW_OR_COLUMN = 16;
+    const NUMBER_OF_SQUARES_IN_ROW_OR_COLUMN = squareCount;
     smallerViewportDimension = Math.floor(smallerViewportDimension / NUMBER_OF_SQUARES_IN_ROW_OR_COLUMN);
 
     //prevent width from shrinking past the minimum width of a square div
-    const MINIMUM_LENGTH_OF_SQUARE_DIV = 10;
+    const MINIMUM_LENGTH_OF_SQUARE_DIV = 3;
     smallerViewportDimension = smallerViewportDimension < MINIMUM_LENGTH_OF_SQUARE_DIV ? MINIMUM_LENGTH_OF_SQUARE_DIV : smallerViewportDimension;
 
     //create 16 columns (vertical) each containing a row (horizontal) of 16 square divs,
     const container = document.querySelector(`#gridContainer`);
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < NUMBER_OF_SQUARES_IN_ROW_OR_COLUMN; i++) {
         const col = document.createElement("div");
         
         //max width makes it so that the square div does not grow past viewport size
         col.style.cssText = `display: flex; flex-direction: column; flex: 1; max-width: ${smallerViewportDimension}px;`;
   
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j < NUMBER_OF_SQUARES_IN_ROW_OR_COLUMN; j++) {
             const row = document.createElement("div");
             const rowSquare = squareDiv.cloneNode();
             row.appendChild(rowSquare);
@@ -39,7 +39,7 @@ function createGrid () {
     }
 }
 
-createGrid();
+createGrid(100);
 
 //select all square divs and make it so they turn black when hovered on
 const squareDiv = document.querySelectorAll(`.box`);
