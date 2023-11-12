30c106dd45d96a085e24f074bad0dda998b63617
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-11-12 04:26:53 -0800

    Darken Colored Box by 10% on Hover
    Put color change box code into function for "cleaner" code. Add data attribute when randomizing RGB to keep track of original RGB values. Add data attribute to every box with initial dark percentage of 0. Add function which darkens box by 10% of original RGB values using data attributes of box

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function createGrid (squareCount = 16) {
     const squareDiv = document.createElement(`div`);
     squareDiv.style.cssText = `border: 1px solid; border-color: black; min-width: 3px; background-color: white; aspect-ratio: 1/1;`;
     squareDiv.classList.add(`box`);
+    squareDiv.dataset.darkenPercent = 0;
 
     //have to later subtract button heihgt from vertical viewport because it affects space grid can fit in (width does not affect this)
     const BUTTON_HEIGHT_AND_MARGIN = 30;
@@ -46,24 +47,65 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
 
-function getRandomRGB() {
+function getRandomRGB(box) {
     const MAX_RGB_VALUE = 255;
     let redValue = getRandomInt(MAX_RGB_VALUE);
     let greenValue = getRandomInt(MAX_RGB_VALUE);
     let blueValue = getRandomInt(MAX_RGB_VALUE);
 
+    //stpre original (may be darkened later) RGB values
+    box.dataset.originalRed = redValue;
+    box.dataset.originalGreen = greenValue;
+    box.dataset.originalBlue = blueValue;
+
     return `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 }
 
+//Darkens box color by 10%-
+function darkenColor(box) {
+    let darkenPercent = Number(box.dataset.darkenPercent);
+
+    //darken by 10 percent
+    darkenPercent += 10;
+    box.dataset.darkenPercent = darkenPercent;
+
+    const NUMBER_OF_TIMES_TO_DARKEN_BY_10_PERCENT = darkenPercent / 10;
+
+    const ORIGINAL_RED_VALUE = Number(box.dataset.originalRed);
+    const ORIGINAL_GREEN_VALUE = Number(box.dataset.originalGreen);
+    const ORIGINAL_BLUE_VALUE = Number(box.dataset.originalBlue);
+
+    //get ten percent of original RGB values rounded down
+    let tenPercentOriginalRed = Math.floor(ORIGINAL_RED_VALUE * 0.1);
+    let tenPercentOriginalGreen = Math.floor(ORIGINAL_GREEN_VALUE * 0.1);
+    let tenPercentOriginalBlue = Math.floor(ORIGINAL_BLUE_VALUE * 0.1);
+
+    let darkenedRedValue = ORIGINAL_RED_VALUE - (tenPercentOriginalRed * NUMBER_OF_TIMES_TO_DARKEN_BY_10_PERCENT);
+    let darkenedGreenValue = ORIGINAL_GREEN_VALUE - (tenPercentOriginalGreen * NUMBER_OF_TIMES_TO_DARKEN_BY_10_PERCENT);
+    let darkenedBlueValue = ORIGINAL_BLUE_VALUE - (tenPercentOriginalBlue * NUMBER_OF_TIMES_TO_DARKEN_BY_10_PERCENT);
+
+    //makes sure that after 10 hovers on colored box will result in black box (have to do this since rounding makes it only close to RGB 0,0,0)
+    if (darkenPercent == 100) {
+        box.style.backgroundColor = `rgb(0, 0, 0)`;
+    } else {
+        box.style.backgroundColor = `rgb(${darkenedRedValue}, ${darkenedGreenValue}, ${darkenedBlueValue}`;
+    }
+}
+
+function changeColor(box) {
+    let randomColor = getRandomRGB(box);
+    box.style.backgroundColor = randomColor;
+    box.classList.add(`finalColor`);
+}
 //select all square divs and make it so they turn random color (rgb) when hovered on
 function changeColorOnHover() {
     let squareDiv = document.querySelectorAll(`.box`);
     squareDiv.forEach((box) => {
         box.addEventListener(`mouseover`, () => {
             if (!box.classList.contains(`finalColor`)) {
-                let randomColor = getRandomRGB();
-                box.style.backgroundColor = randomColor;
-                box.classList.add(`finalColor`);
+                changeColor(box);
+            } else if (Number(box.dataset.darkenPercent) < 100) {
+                darkenColor(box);
             }
         });
     });
